e114c532aaf7c7d228877ec7392d14011c9d865c
Wallaal06 <62452809+Wallaal06@users.noreply.github.com>
2020-12-15 19:49:20 -0600

    new

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -62,8 +62,8 @@
  *          6: MAGENTA,    7: YELLOW,      8: ORANGE,  9: PURPLE,   10: LIME,  11: BROWN,
  *         12: DARKBLUE,  13: DARKGREEN,  14: GRAY,   15: DARKGRAY
  */
-#define MARLIN_BKCOLOR 4
-#define MARLIN_FNCOLOR 0
+#define MARLIN_BKCOLOR 1
+#define MARLIN_FNCOLOR 4
 
 /**
  * Text displayed at the top of the TFT in Marlin Mode.
